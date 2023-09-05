324cfdb9e0a4fc3805cfd590fe78fa0f99ef32f2
Eder Souza <https://github.com/EderSouza93>
2023-09-05 12:48:01 -0300

    Add functions bill-water and bill-energy

--- src/script.js
+++ src/script.js
@@ -2,8 +2,12 @@
 const contractEndDateInput = document.getElementById("date-end");
 const waterBillValueInput = document.getElementById("water-bill-value");
 const waterReadingDateInput = document.getElementById("water-reading-date");
+const resultWater = document.getElementById("result-water");
+const differenceDaysWaterBtn = document.getElementById("difference-days-water");
 const energyBillValueInput = document.getElementById("energy-bill-value");
 const energyReadingDateInput = document.getElementById("energy-reading-date");
+const resultEnergy = document.getElementById("result-energy");
+const differenceDaysEnergyBtn = document.getElementById("difference-days-energy");
 const condominiumBillValueInput = document.getElementById(
   "condominium-bill-value"
 );
@@ -12,17 +16,20 @@ const iptuBillValueInput = document.getElementById("iptu-bill-value");
 const iptuPaymentInput = document.getElementById("iptu-payment");
 const spuBillValueInput = document.getElementById("spu-bill-value");
 const spuPaymentInput = document.getElementById("spu-payment");
-const differenceDays = document.getElementById("difference-days");
-const results = document.getElementById("result-water");
+
+
 
 const currentYear = new Date().getFullYear();
 const currentTime = new Date(`January 01 ${currentYear}`);
 
-const differenceBetweenDays = () => {
-  //manipulando os inputs
-  const contractEndDate = contractEndDateInput.value;
+
+
+const differenceBetweenDaysWater = () => {
+  //manipulando os inputs 
   const waterReadingDate = waterReadingDateInput.value;
   const waterBillValue = waterBillValueInput.value;
+  const contractEndDate = contractEndDateInput.value;
+  
 
   //convertendo as datas para objetos
   const data1 = new Date(contractEndDate);
@@ -30,11 +37,34 @@ const differenceBetweenDays = () => {
 
   const diffTime = Math.abs(data2 - data1);
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24) + 1);
-
+  // Calculando o valor a ser ressarcido de água
   const valueWater = (waterBillValue / 30) * diffDays;
-  console.log(valueWater);
-  results.textContent = `O valor é ${valueWater}`;
-  console.log(currentTime);
+  console.log(contractEndDate);
+  resultWater.textContent = `O valor é ${valueWater}`;
 };
 
-differenceDays.addEventListener("click", differenceBetweenDays);
+differenceDaysWaterBtn.addEventListener("click", differenceBetweenDaysWater);
+
+const differenceBetweenDaysEnergy = () => {
+  // manipulando os inputs 
+  const energyReadingDate = energyReadingDateInput.value;
+  const energyBillValue = energyBillValueInput.value;
+  const contractEndDate = contractEndDateInput.value;
+  
+  // convertendo as datas para objetos
+  const data1 = new Date(contractEndDate);
+  const data2 = new Date(energyReadingDate);
+
+  // Calculando a diferença de datas
+  const diffTime = Math.abs(data2 - data1);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24) + 1);
+
+  const valueEnergy = (energyBillValue / 30) * diffDays;
+  resultEnergy.textContent = `O valor à ser pago é ${valueEnergy}`;
+};
+
+differenceDaysEnergyBtn.addEventListener("click", differenceBetweenDaysEnergy)
+
+const calculateCondomium = () => {
+  
+}
