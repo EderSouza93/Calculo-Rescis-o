961e2c4212536616399d3f44107f191f38374581
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2025-01-05 13:01:45 -0300

    Move CRM refactor files to project root

--- src/utils/validations.js
+++
@@ -1,8 +0,0 @@
-export const validateInputs = (...inputs) => {
-    for (const inputs of inputs) {
-        if(isNaN(inputs) || inputs <= 0){
-            return { sucess: false, message: "Insira dados válidos."};
-        }
-    }
-    return { sucess: true };
-}
